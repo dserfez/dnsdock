aa512218ca970843462c06a54138cd770324bf55
Davor <dserfez@gmail.com>
2016-07-13 09:51:34 +0200

    Changed listening port

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+sudo iptables -t nat -A PREROUTING -s 172.17.42.0/24 -d 172.17.42.1 -p udp --dport 53 -j REDIRECT --to 5553
+
 docker run --rm -ti --name dns \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -p 172.17.42.1:53:53/udp \
+  -p 172.17.42.1:5553:53/udp \
   cycomf/dnsdock
